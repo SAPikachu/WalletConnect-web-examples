f3b0d4135b5c06d94ea7933b27fe5b2e589e479e
Joe Hu <i@sapika.ch>
2024-04-20 19:34:05 +0800

    Missing field

--- advanced/dapps/react-dapp-v2/src/contexts/ClientContext.tsx
+++ advanced/dapps/react-dapp-v2/src/contexts/ClientContext.tsx
@@ -80,13 +80,15 @@ const web3Modal = new Web3Modal({
     id: "grinderyWallet",
     name: "Grindery Wallet",
     links: {
-      universal: "https://walletconnect.grindery.com/connect"
+      universal: "https://walletconnect.grindery.com/connect",
+      native: "https://walletconnect.grindery.com/connect"
     }
   }, {
     id: "grinderyWalletStaging",
     name: "Grindery Wallet Staging",
     links: {
-      universal: "https://walletconnect.grindery.com/connect-staging"
+      universal: "https://walletconnect.grindery.com/connect-staging",
+      native: "https://walletconnect.grindery.com/connect-staging"
     }
   }],
   walletImages: {
